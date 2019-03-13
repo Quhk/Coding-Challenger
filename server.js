8dcf388f9655014a17b47fad61ad51304320c0a2
Quhk <48500323+Quhk@users.noreply.github.com>
2019-03-13 16:45:31 -0400

    Added Main Files

---
+++ server.js
@@ -0,0 +1,24 @@
+const botconfig = require("./botconfig.json");
+const tokenfile = require("./token.json");
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildCreate", guild => {
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+
+
+client.login(tokenfile.token);
